0e04708d84cf9735aa28aa8a2f453db150047574
Albin Sunnanbo <albin@sunnanbo.com>
2016-10-28 18:46:25 +0200

    Include a sample version of the output. Normally you add the AssemblyInfo.VersionNumber.cs to the project file, but don't keep the file in version control since it is recreated on every build
    Need to remove AssemblyVersion and AssemblyFileVersion from AssemblyInfo.cs

---
+++ AssemblyVersionFromGit/Properties/AssemblyInfo.VersionNumber.cs
@@ -0,0 +1,5 @@
+using System.Reflection;
+
+[assembly: AssemblyVersion("1.0.0.3")]
+[assembly: AssemblyFileVersion("1.0.0.3")]
+[assembly: AssemblyInformationalVersion("3.b8472d6c0ca5730c94eed6cba8c67e6d3ad571ce")]
